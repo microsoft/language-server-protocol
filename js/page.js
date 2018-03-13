71c4420657f686887648eee6b1e79c412dc83d37
Pine Wu <octref@gmail.com>
2018-03-13 12:03:37 -0700

    Add GA and update cookie handling

--- js/page.js
+++ js/page.js
@@ -1,3 +1,26 @@
-  $("#small-nav-dropdown").change(function() {
-    window.location = $(this).find("option:selected").val();
-  });  
+$('#small-nav-dropdown').change(function() {
+  window.location = $(this)
+    .find('option:selected')
+    .val()
+})
+
+$(function() {
+  if (mscc) {
+    if (!mscc.hasConsent()) {
+      window.addEventListener('click', function() {
+        if (!mscc.hasConsent()) {
+          mscc.setConsent()
+        }
+      })
+
+      mscc.on('consent', function() {
+        window.dataLayer = window.dataLayer || []
+        function gtag() {
+          dataLayer.push(arguments)
+        }
+        gtag('js', new Date())
+        gtag('config', 'UA-62780441-30', { anonymize_ip: true })
+      })
+    }
+  }
+})
