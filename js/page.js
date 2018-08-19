83259d91ad2454961e981e63728333457457e79e
Pine Wu <octref@gmail.com>
2018-08-18 21:27:03 -0700

    WIP for page anchor

--- js/page.js
+++ js/page.js
@@ -22,8 +22,38 @@ $(function() {
       })
 
       // Where future non-essential tracking cookie need to go
-      mscc.on('consent', function() {
-      })
+      mscc.on('consent', function() {})
     }
   }
 })
+
+var anchorForId = function(id) {
+  var anchor = document.createElement('a')
+  anchor.className = 'header-link'
+  anchor.href = '#' + id
+  anchor.innerHTML = '<i class="fa fa-link"></i>'
+  return anchor
+}
+
+var linkifyAnchors = function(level, containingElement) {
+  var headers = containingElement.getElementsByTagName('h' + level)
+  for (var h = 0; h < headers.length; h++) {
+    var header = headers[h]
+
+    if (typeof header.id !== 'undefined' && header.id !== '') {
+      header.appendChild(anchorForId(header.id))
+    }
+  }
+}
+
+document.onreadystatechange = function() {
+  if (this.readyState === 'complete') {
+    var contentBlock = document.getElementById('markdown-content-container')
+    if (!contentBlock) {
+      return
+    }
+    for (var level = 2; level <= 4; level++) {
+      linkifyAnchors(level, contentBlock)
+    }
+  }
+}
